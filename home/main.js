b7ade1929ac26cde17302438902bb20d0f85dcf7
RenamS <renams.alves@gmail.com>
2022-05-30 20:06:21 -0300

    add estrelas para favoritar jogos

--- home/main.js
+++ home/main.js
@@ -1,74 +1,180 @@
 const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com',
-		'X-RapidAPI-Key': 'cf1bbb1a8emsh5d0bf67025b2255p1bf63djsn54f95fd8dedd'
-	}
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com',
+        'X-RapidAPI-Key': '94bd9a0258mshbb54c719823d4d2p19082ajsn4760e6d66c6a'
+    }
 };
-var main_page = document.getElementById("main_page")
 
-console.log(main_page)
-let num = 0
+const balaogrande = document.getElementById("balaogrande")
+const balaopequeno = document.getElementById("balaocentro")
+var jogos = []
+let contagem = 0;
+let atual = ''
+function limparTela() {
+    document.getElementById("paicentral").innerHTML = '';
 
-var card_text = [10]
-var card_img = [10]
-var card1=[10]
-	
-const request = async() => {
-	const obj = []
+    const myNode = document.getElementById("paibalaomenor");
+    while (myNode.firstChild) {
+        myNode.removeChild(myNode.lastChild);
+    }
+}
 
-    const url = 'https://free-to-play-games-database.p.rapidapi.com/api/games?platform=pc';
-    fetch(url, options)
-    const dados = await fetch(url);
-    const jogos = await dados.json();
-	console.log(jogos);
+async function Todos() {
+    limparTela();
+    jogos = await pegarTodos()
+    atual = 'all'
+    contagem = 0
+    colocarJogosNaTela(jogos)
+}
 
-	for(let x=0; x < 10; x++){
-		obj[x] = jogos[x]
-		console.log(obj[x])
-		// console.log(obj[x].title)
+async function Browser() {
+    limparTela();
+    jogos = await pegarBrowser()
+    atual = 'browser'
+    contagem = 0
+    colocarJogosNaTela(jogos)
+}
 
-		card_text[x]= document.createElement("p")
-		card_img[x]= document.createElement("img")
+// funções das modalidades
 
-		card_img[x].src = obj[x].thumbnail
-		card_img[x].appendChild(document.createTextNode(obj[x].thumbnail))
-		card_text[x].appendChild(document.createTextNode(obj[x].title))
-		
-		console.log(card_img[x])
-		console.log(card_text[x])
+async function pvp() {
+    limparTela()
+    jogos = await pegarPvp(atual)
+    contagem = 0
+    colocarJogosNaTela(jogos)
+}
 
-		card1[x] = document.createElement("div")
-
-		card1[x].appendChild(card_img[x])
-		card1[x].appendChild(card_text[x])
-
-		main_page.appendChild(card1[x])
-		
-		// console.log(card1[x])
-
-	}
-
-	if(card1[0]){
-		card1[0].classList.remove("card1")
-		card1[0].classList.add("baner")
-		
-
-	}
-
-	// card1[0].classList.add("card1")
-	card1[1].classList.add("card2")
-	card1[2].classList.add("card3")
-	card1[3].classList.add("card4")
-	card1[4].classList.add("card5")
-	card1[5].classList.add("card6")
-	card1[6].classList.add("card7")
-	card1[7].classList.add("card8")
-	card1[8].classList.add("card9")
-	card1[9].classList.add("card10")
+async function mmofps() {
+    limparTela();
+    jogos = await pegarmmofps(atual);
+    contagem = 0;
+    colocarJogosNaTela(jogos)
+}
 
 
-}	
-main_page.classList.add("main_page_div")
-request();
+async function pegarmmofps(atual) {
+    console.log(`https://free-to-play-games-database.p.rapidapi.com/api/games?platform=${atual}&category=mmofps`)
+    return fetch(`https://free-to-play-games-database.p.rapidapi.com/api/games?platform=${atual}&category=mmofps`, options)
+        .then(response => response.json())
+        .then(response => {
+            return response
+        })
+        .catch(err => console.error(err));
+}
 
+
+
+
+
+
+async function pegarPvp(atual) {
+    console.log(`https://free-to-play-games-database.p.rapidapi.com/api/games?platform=${atual}&category=pvp`)
+    return fetch(`https://free-to-play-games-database.p.rapidapi.com/api/games?platform=${atual}&category=pvp`, options)
+        .then(response => response.json())
+        .then(response => {
+            return response
+        })
+        .catch(err => console.error(err));
+}
+
+function colocarJogosNaTela(jogosNaTela = jogos) {
+
+    if (contagem == 0) {
+        const cloneBalaoGrande = balaogrande.cloneNode(true);
+
+        cloneBalaoGrande.getElementsByTagName("h2")[0].innerHTML = jogosNaTela[0].title
+        cloneBalaoGrande.setAttribute("style", `background-image: url(${jogosNaTela[0].thumbnail}); `)
+        cloneBalaoGrande.style.display = 'flex'
+
+        document.getElementById("paicentral").appendChild(cloneBalaoGrande)
+        contagem++;
+    }
+
+
+    for (let i = contagem; i <= contagem + 8; i++) {
+        const cloneBalaoPequeno = balaopequeno.cloneNode(true)
+
+        cloneBalaoPequeno.getElementsByTagName("h2")[0].innerHTML = jogosNaTela[i].title
+        cloneBalaoPequeno.setAttribute("style", `background: url(${jogosNaTela[i].thumbnail}); background-size: cover;`)
+        cloneBalaoPequeno.style.display = 'flex'
+
+        document.getElementById("paibalaomenor").appendChild(cloneBalaoPequeno)
+    }
+
+    contagem += 9;
+}
+
+
+function limparTela() {
+    document.getElementById("paicentral").innerHTML = '';
+    let i = 1
+
+    const myNode = document.getElementById("paibalaomenor");
+    while (myNode.firstChild) {
+        myNode.removeChild(myNode.lastChild);
+    }
+
+}
+
+async function pegarTodos() {
+    return fetch('https://free-to-play-games-database.p.rapidapi.com/api/games', options)
+        .then(response => response.json())
+        .then(response => {
+            return response
+        })
+        .catch(err => console.error(err));
+}
+
+
+async function Plataforms() {
+    return await fetch('https://free-to-play-games-database.p.rapidapi.com/api/games?platform=all')
+        .then(response => response.json())
+        .then(response => {
+            return response
+        })
+        .catch(err => console.error(err));
+}
+async function PC() {
+    return await fetch('https://free-to-play-games-database.p.rapidapi.com/api/games?platform=pc')
+        .then(response => response.json())
+        .then(response => {
+            return response
+        })
+        .catch(err => console.error(err));
+}
+
+// colocarJogosNaTela(jogos);
+
+
+
+async function pegarBrowser() {
+    return await fetch('https://free-to-play-games-database.p.rapidapi.com/api/games?platform=browser', options)
+        .then(response => response.json())
+        .then(response => {
+            console.log(response)
+            return response
+        })
+        .catch(err => console.error(err));
+}
+// MENU LATERAL
+
+const list = document.querySelectorAll('.list');
+function activeLink() {
+    list.forEach((item) =>
+        item.classList.remove('active'));
+    this.classList.add('active');
+}
+list.forEach((item) =>
+    item.addEventListener('click', activeLink));
+
+// adicionar jogo aos fav ao clicar
+let favorites = JSON.parse(localStorage.getItem('favorites')) || []
+
+document.querySelector('.balaocentro').onclick = function () {
+
+    const imageSource = document.querySelector('.balaocentro').src
+
+    favorites.push(imageSource)
+    localStorage.setItem('favorites', JSON.stringify(favorites))
+}
